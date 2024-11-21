7081a689b1f2bb8fa97ef43a86eda149562b9ec5
Dan <wanang@gmail.com>
2024-11-21 17:30:09 +0800

    Added read functionality to display task

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include 'db.php';  // Include the database connection
 
 $sql = "SELECT * FROM tasks ORDER BY created_at DESC";  // Query to get tasks ordered by creation time
 $result = $conn->query($sql);  // Execute the query
-
 ?>
 <!DOCTYPE html>
 <html>
